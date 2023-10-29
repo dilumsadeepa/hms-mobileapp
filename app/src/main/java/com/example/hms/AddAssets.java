799269e1d3e2fb1548cac6b7bac66a6dcd20f0a9
Firstname Lastname <101855974+Avishkasj@users.noreply.github.com>
2023-10-29 18:46:00 +0530

    qr start

--- app/src/main/java/com/example/hms/AddAssets.java
+++ app/src/main/java/com/example/hms/AddAssets.java
@@ -3,12 +3,32 @@ package com.example.hms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddAssets extends AppCompatActivity {
+
+    String[] categoryval= {"Furniture","Electronic","Optional"};
+    String[] hostal= {"Girls","Boys"};
+
+    EditText noassets;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assets);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categoryval);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,hostal);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
     }
 }
