388d316ac1bb31aa7af001713214a905c3b99563
Firstname Lastname <101855974+Avishkasj@users.noreply.github.com>
2023-10-30 18:19:45 +0530

    only work emu

--- app/src/main/java/com/example/hms/Viewitem.java
+++ app/src/main/java/com/example/hms/Viewitem.java
@@ -2,25 +2,105 @@ package com.example.hms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Viewitem extends AppCompatActivity {
 
-    TextView tv;
-    String resultData="";
+    TextView name;
+    TextView room;
+    TextView idate;
+    TextView mdate;
+    TextView status;
+
+    private static final String API_URL = "http://192.168.8.126:8080/res/find/1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewitem);
 
+        name = findViewById(R.id.iname);
+        room = findViewById(R.id.iroomno);
+        idate = findViewById(R.id.iidate);
+        mdate = findViewById(R.id.ilmdate);
+        status = findViewById(R.id.istatus);
 
-        tv = findViewById(R.id.itemid);
-        Intent intent = getIntent();
-        resultData = intent.getStringExtra("result_data");
+        new APICallTask().execute();
+    }
 
-        tv.setText(resultData);
+    private class APICallTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                int responseCode = connection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    return result.toString();
+                } else {
+                    // Handle the error case
+                    return "Error: " + responseCode;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Error: " + e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonResponse = new JSONObject(result);
+
+                // Extract values from the JSON response
+                String nameValue = jsonResponse.getString("resName");
+                String roomValue = jsonResponse.getString("roomNo");
+                String idateValue = jsonResponse.getString("installationDate");
+                String mdateValue = jsonResponse.getString("lastMaintenanceDate");
+                String statusValue = jsonResponse.getString("status");
+
+                // Set the values to the corresponding TextViews
+                name.setText(nameValue);
+                room.setText(roomValue);
+                idate.setText(idateValue);
+                mdate.setText(mdateValue);
+                status.setText(statusValue);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                // Handle the JSON parsing error here
+                showErrorMessage("JSON Parsing Error");
+            }
+        }
+
+        // Display an error message to the user
+        private void showErrorMessage(String message) {
+            Toast.makeText(Viewitem.this, message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
